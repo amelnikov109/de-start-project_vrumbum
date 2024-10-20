bf176e7b74de6f7cd36d07aab68c105c2ba9fb92
amelnikov109 <amelnikov109@gmail.com>
2024-10-20 18:04:40 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,5 @@
 /*добавьте сюда запрос для решения задания 1*/
+SELECT 
+	(COUNT(*) FILTER (WHERE gasoline_consumption is NULL)::REAL / COUNT(*) * 100)::NUMERIC(4, 2) as nulls_percentage_gasoline_consumption
+  FROM car_shop.model
+ ;
