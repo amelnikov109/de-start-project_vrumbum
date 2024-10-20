1004715439d3dd3a693bd5bee6be02f44c087527
amelnikov109 <amelnikov109@gmail.com>
2024-10-20 18:05:22 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,9 @@
 /*добавьте сюда запрос для решения задания 3*/
+SELECT EXTRACT(MONTH FROM CAST(i.invoice_date AS timestamp)) AS month,
+     EXTRACT(YEAR FROM CAST(i.invoice_date AS timestamp)) AS year,
+     ROUND(AVG(price), 2) AS price_avg
+  FROM car_shop.invoice i
+  WHERE EXTRACT (YEAR FROM CAST(i.invoice_date AS timestamp)) = '2022'
+  GROUP BY month, year
+  ORDER BY month
+ ;
