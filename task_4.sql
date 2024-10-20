71934ed0340695364ef1f434d3d1842bd7531014
amelnikov109 <amelnikov109@gmail.com>
2024-10-20 18:05:39 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,11 @@
 /*добавьте сюда запрос для решения задания 4*/
+SELECT c.name AS person,
+     STRING_AGG(CONCAT(b.name, ' ', m.model_name), ', ') AS cars
+  FROM car_shop.invoice i
+   JOIN car_shop.car USING(car_id)
+   JOIN car_shop.model m USING(model_id)
+   JOIN car_shop.brand b USING(brand_id)
+   JOIN car_shop.customer c USING(customer_id)
+  GROUP BY person
+  ORDER BY person
+ ;
