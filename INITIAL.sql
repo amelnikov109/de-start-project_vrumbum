b22b582e7dd51988aa8bcd11678d9b4298ff621e
amelnikov109 <amelnikov109@gmail.com>
2024-10-20 18:01:44 +0300

    Update INITIAL.sql

--- INITIAL.sql
+++ INITIAL.sql
@@ -1,1 +1,17 @@
 /*сохраните в этом файле запросы для первоначальной загрузки данных - создание схемы raw_data и таблицы sales и наполнение их данными из csv файла*/
+
+create schema if not exists raw_data;
+create table if not exists raw_data.sales
+(
+    id                      integer,
+    auto                    text,
+    gasoline_consumption    text,
+    price                   numeric(9,2),
+    date                    date,
+    person_name             text,
+    phone                   text,
+    discount                smallint default 0,
+    brand_origin            text
+);
+
+copy raw_data.sales from 'C:\Temp\cars.csv' with csv header null 'null';
